e774a9066347efd79c0c5e104ff5916f6f1e7fd7
Neil J Martin <neilm@4js.com>
2017-09-19 12:21:06 +0100

    ppt draft

--- gbc/gbc-njm/js/MyFormWidget.js
+++ gbc/gbc-njm/js/MyFormWidget.js
@@ -1,3 +1,12 @@
+/// FOURJS_START_COPYRIGHT(D,2014)
+/// Property of Four Js*
+/// (c) Copyright Four Js 2014, 2016. All Rights Reserved.
+/// * Trademark of Four Js Development Tools Europe Ltd
+///	 in the United States and elsewhere
+/// 
+/// This file can be modified by licensees according to the
+/// product manual.
+/// FOURJS_END_COPYRIGHT
 
 modulum('MyFormWidget', ['FormWidget', 'WidgetFactory'],
 	function(context, cls) {
