1531b0f21b6045d2a2132d799c7fb19ef8e924a9
Neil J Martin <neilm@4js.com>
2018-02-01 10:09:06 +0000

    gdc proj dir

--- env.sh
+++ env.sh
@@ -4,3 +4,5 @@ export PROJBASE=$( pwd )
 export FGLIMAGEPATH=$PROJBASE/pics:$FGLDIR/lib/image2font.txt
 export MUSICDIR=$PROJBASE/Music
 export FGLRESOURCEPATH=$PROJBASE/etc
+
+export GBCPROJDIR=/opt/fourjs/gbc-current-project
