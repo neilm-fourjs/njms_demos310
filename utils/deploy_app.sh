19c2d172e22e31b71099c2d821b6b6629d830b67
Neil Martin <neilm@4js-emea.com>
2019-12-09 15:30:40 +0000

    .sh

--- utils/deploy_app.sh
+++ utils/deploy_app.sh
@@ -1,43 +1,104 @@
 #!/bin/bash
-# needs to run using: sudo -u gpaas ./deploy_app.sh <appname>
+# If the user doesn't have permissions to deploy apps then use sudo
 
-VER=${2:-310}
+function localDeploy() {
+	GAR=$1
+	VER=$2
 
-# Set the GAS environment
-. /opt/fourjs/gas$VER/envas
+	# Set the GAS environment
+	. /opt/fourjs/gas$VER/envas
 
-# Define the command using our custom XCF
-if [ -e $FGLASDIR/etc/isv_as$VER.xcf ]; then
-	CMD="gasadmin gar -f $FGLASDIR/etc/isv_as$VER.xcf"
+	# Define the command using our custom XCF
+	if [ -e $FGLASDIR/etc/isv_as$VER.xcf ]; then
+		CMD="gasadmin gar -f $FGLASDIR/etc/isv_as$VER.xcf"
+	else
+		CMD="gasadmin gar"
+	fi
+	
+	echo -e "\n attempt to disable previous version of app ..."
+	echo "$CMD --disable-archive $GAR"
+	$CMD --disable-archive $GAR
+	if [ $? -eq 0 ]; then
+		echo -e "\n attempt to undeploy previous version of app ..."
+		echo	"$CMD --undeploy-archive $GAR"
+		$CMD --undeploy-archive $GAR
+	fi
+	
+	#echo -e "\n List archives:"
+	#$CMD --list-archives
+	
+	echo -e "\n attempt to clean archives ..."
+	echo "$CMD --clean-archives"
+	$CMD --clean-archives
+	
+	echo -e "\n attempt to install new version of app ..."
+	echo "$CMD --deploy-archive $GAR.gar"
+	$CMD --deploy-archive $GAR.gar
+	
+	if [ $? -eq 0 ]; then
+		echo -e "\n attempt to enable app ..."
+		echo "$CMD --enable-archive $GAR"
+		$CMD --enable-archive $GAR
+	else
+		echo "Deploy Failed!"
+	fi
+}
+
+function remoteDeploy() {
+	GAR=$1
+	VER=$2
+	CMD="./deploy_app.sh 0 $GAR $VER"
+
+	echo "Deploying ${GAR}.gar to $HOST ..."
+	# Copy the gar to the server
+	scp -P $PORT ${GAR}.gar $HOST:
+	# Run the deploy script to use gasadmin to re-deploy the gar
+	ssh -p $PORT $HOST $CMD
+}
+
+# Main code
+
+PORT=22
+SRV=${1:-1}
+APP=${2:-njms_demos$VER}
+VER=${3:-320}
+DB=pgs
+GAR=${APP}_${DB}
+
+if [ ! -e ${GAR}_${DB}.gar ]; then
+	exit 1
+fi
+
+
+case $SRV in
+0)
+	HOST=local
+;;
+1)
+	HOST=$4
+	DB=${5:-ifx}
+;;
+2)
+	HOST=pi@generodemos.dynu.net
+	PORT=666
+;;
+3)
+	HOST=ryan-4js.com
+	PORT=666
+;;
+4)
+	HOST=neilm@demos.4js-emea.com
+;;
+5)
+	HOST=neilm@10.2.1.100
+;;
+esac
+
+if [ "$HOST" == "local" ] || [ "$HOST" == "localhost" ]; then
+	localDeploy $GAR $VER
 else
-	CMD="gasadmin gar"
+	remotelDeploy $GAR $VER
 fi
 
-echo -e "\n attempt to disable previous version of app ..."
-echo "$CMD --disable-archive $1"
-$CMD --disable-archive $1
-if [ $? -eq 0 ]; then
-	echo -e "\n attempt to undeploy previous version of app ..."
-	echo  "$CMD --undeploy-archive $1"
-	$CMD --undeploy-archive $1
-fi
-
-#echo -e "\n List archives:"
-#$CMD --list-archives
-
-echo -e "\n attempt to clean archives ..."
-echo "$CMD --clean-archives"
-$CMD --clean-archives
-
-echo -e "\n attempt to install new version of app ..."
-echo "$CMD --deploy-archive $1.gar"
-$CMD --deploy-archive $1.gar
-
-if [ $? -eq 0 ]; then
-	echo -e "\n attempt to enable app ..."
-	echo "$CMD --enable-archive $1"
-	$CMD --enable-archive $1
-else
-	echo "Deploy Failed!"
-fi
+echo "Finished."
 
--- utils/deploy_gbc.sh
+++ utils/deploy_gbc.sh
@@ -1,26 +1,82 @@
 #!/bin/bash
 
-# needs to run using: sudo -u gpaas ./deploy_gbc.sh <name>
+# Simple script to copy & deploy a custom GBC .zip file
+# ./deploy_gbc.sh  # No args = deploy to default server with 320
+# ./deploy_gbc.sh 0  # deploy to localhost with 320
+# ./deploy_gbc.sh x fourjs@oldserver.com 310  # deploy to a specific server with 310
 
-VER=${2:-310}
+function localDeploy() {
+	GBC=$1
+	GASVER=$2
 
-# Set the GAS environmment
-. /opt/fourjs/gas$VER/envas
-gasadmin -V
+	# Set the GAS environmment
+	. /opt/fourjs/gas$VER/envas
+	gasadmin -V
 
-# Define the command with custom .xcf file
-if [ -e $FGLASDIR/etc/isv_as$VER.xcf ]; then
-	CMD="gasadmin gbc -f $FGLASDIR/etc/isv_as$VER.xcf"
-else
-	CMD="gasadmin gbc"
+	# Define the command with custom .xcf file if it exists
+	if [ -e $FGLASDIR/etc/isv_as$VER.xcf ]; then
+  	CMD="gasadmin gbc -f $FGLASDIR/etc/isv_as$VER.xcf"
+	else
+  	CMD="gasadmin gbc"
+	fi
+	
+	echo "Attempt to undeploy previous version of gbc ..."
+	echo "$CMD --undeploy $GBC"
+	$CMD --undeploy $GBC
+	
+	echo "Attempt to deploy new version of gbc ..."
+	echo "$CMD --deploy $GBC"
+	$CMD --deploy $GBC
+	
+	#rm $GBC
+}
+
+function remoteDeploy() {
+	GBC=$1
+	GASVER=$2
+	# The ./deploy_gbc.sh on target machine sets the correct GAS env and runs the 
+	# gasadmin command to deploy the custom GBC
+	CMD="./deploy_gbc.sh $GBC $VER"
+	
+	# Copy the gar to the server
+	echo "Coping ${GBC} to $HOST ..."
+	scp ${GBC} $HOST:
+
+	# Run CMD to deploy the copied GBC
+	echo "Running ${CMD} on $HOST ..."
+	ssh $HOST $CMD
+}
+
+SRV=${1:-1}
+VER=${2:-320}
+
+case $SRV in
+0)
+  HOST=local
+;;
+1)
+  HOST=fourjs@myserver.com
+;;
+2)
+  HOST=fourjs@myserver2.com
+;;
+*)
+	HOST=$3
+;;
+esac
+
+if [ $(ls -l gbc-*.zip | wc -l) -eq 0 ]; then
+	echo "No GBC zip files found to deploy!"
+	exit 1
 fi
 
-echo "Attempt to undeploy previous version of gbc ..."
-echo "$CMD --undeploy $1"
-$CMD --undeploy $1
+for GBC in gbc-*.zip
+do
+	if [ "$HOST" == "local" ]; then
+		localDeploy $GBC $VER
+	else
+		remotelDeploy $GBC $VER
+	fi
+done
 
-echo "Attempt to deploy new version of gbc ..."
-echo "$CMD --deploy $1"
-$CMD --deploy $1
-
-rm $1
+echo "Finished."
